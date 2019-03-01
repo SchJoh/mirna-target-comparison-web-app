c54cf0e0a3282452dae7b376386c26c4a21f0fb4
Johannes Schramm <johannesschramm@client0690.vpn.uni-saarland.de>
2019-03-01 11:13:29 +0100

    initital push

---
+++ src/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/NavBar.css';
+
+const NavBar = () => (
+    <div className="NavBar">
+        <div className="logo ms-font-xl">
+            <strong>miRNA binding site alignment</strong>
+        </div>
+    </div>
+);
+
+export default NavBar;
